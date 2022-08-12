ea20f96246e02a65f0f1533e434e21ea197a2796
william <s3864077@rmit.edu.vn>
2022-08-12 22:17:03 +0700

    All function working

--- src/mbox.c
+++ src/mbox.c
@@ -1,7 +1,10 @@
-// -----------------------------------mbox.c -------------------------------------
+// -----------------------------------mbox.c
+// -------------------------------------
 #include "mbox.h"
+
 #include "gpio.h"
 #include "uart.h"
+
 /* Mailbox Data Buffer (each element is 32-bit)*/
 /*
  * The keyword attribute allows you to specify special attributes
@@ -17,63 +20,66 @@
  * (last 4 bits is ZERO due to 16 byte alignment)
  *
  */
+
 volatile unsigned int __attribute__((aligned(16))) mBuf[36];
+
 /**
  * Read from the mailbox
  */
-uint32_t mailbox_read(unsigned char channel)
-{
+uint32_t mailbox_read(unsigned char channel) {
     // Receiving message is buffer_addr & channel number
     uint32_t res;
-        // Make sure that the message is from the right channel
-        do
-    {
+
+    // Make sure that the message is from the right channel
+    do {
         // Make sure there is mail to receive
-        do
-        {
+        do {
             asm volatile("nop");
         } while (*MBOX0_STATUS & MBOX_EMPTY);
+
         // Get the message
         res = *MBOX0_READ;
-    }
-    while ((res & 0xF) != channel)
-        ;
+    } while ((res & 0xF) != channel);
+
     return res;
 }
+
 /**
  * Write to the mailbox
  */
-void mailbox_send(uint32_t msg, unsigned char channel)
-{
+void mailbox_send(uint32_t msg, unsigned char channel) {
     // Sending message is buffer_addr & channel number
-    //  Make sure you can send mail
-    do
-    {
+
+    // Make sure you can send mail
+    do {
         asm volatile("nop");
     } while (*MBOX1_STATUS & MBOX_FULL);
+
     // send the message
     *MBOX1_WRITE = msg;
 }
+
 /**
  * Make a mailbox call. Returns 0 on failure, non-zero on success
  */
-int mbox_call(unsigned int buffer_addr, unsigned char channel)
-{
+int mbox_call(unsigned int buffer_addr, unsigned char channel) {
     // Check Buffer Address
     uart_puts("Buffer Address: ");
     uart_hex(buffer_addr);
     uart_sendc('\n');
+
     // Prepare Data (address of Message Buffer)
     unsigned int msg = (buffer_addr & ~0xF) | (channel & 0xF);
     mailbox_send(msg, channel);
+
     /* now wait for the response */
     /* is it a response to our message (same address)? */
-    if (msg == mailbox_read(channel))
-    {
+    if (msg == mailbox_read(channel)) {
         /* is it a valid successful response (Response Code) ? */
-        if (mBuf[1] == MBOX_RESPONSE)
-            uart_puts("Got successful response \n");
+        if (mBuf[1] == MBOX_RESPONSE) uart_puts("Got successful response \n");
+
         return (mBuf[1] == MBOX_RESPONSE);
     }
+
     return 0;
 }
--- src/quest_b.c
+++ src/quest_b.c
@@ -248,35 +248,25 @@ int get_arguments(char *str, int m, int n, char args[][n]) {
 
 // Utility
 void get_brdrev() {
-    // mailbox data buffer: Read ARM frequency
     mBuf[0] =
         8 *
-        4;  // Message Buffer Size in bytes (8 elements * 4 bytes (32 bit) each)
+        4;  // Message Buffer Size in bytes (9 elements * 4 bytes (32 bit) each)
     mBuf[1] = MBOX_REQUEST;  // Message Request Code (this is a request message)
-    mBuf[2] = 0x00030002;    // TAG Identifier: Get clock rate
+
+    mBuf[2] = 0x00010002;  // TAG Identifier: Get clock rate
     mBuf[3] =
-        8;  // Value buffer size in bytes (max of request and response lengths)
+        4;  // Value buffer size in bytes (max of request and response lengths)
     mBuf[4] = 0;  // REQUEST CODE = 0
-    mBuf[5] = 3;  // clock id: ARM system clock
-    mBuf[6] = 0;  // clear output buffer (response data are mBuf[5] & mBuf[6])
-    mBuf[7] = MBOX_TAG_LAST;
+    mBuf[5] = 0;  // clear output buffer
+    mBuf[6] = MBOX_TAG_LAST;
 
     if (mbox_call(ADDR(mBuf), MBOX_CH_PROP)) {
-        uart_puts("Response Code for whole message: ");
-        uart_hex(mBuf[1]);
-        uart_puts("\n");
-
-        uart_puts("Response Code in Message TAG: ");
-        uart_hex(mBuf[4]);
-        uart_puts("\n");
-
-        uart_puts("DATA: ARM clock rate = ");
-        uart_dec(mBuf[6]);
-        uart_puts("\n");
-
+        uart_puts("Board revision: ");
+        uart_hex(mBuf[5]);
     } else {
         uart_puts("Unable to query!\n");
     }
+    
 }
 // Utility
 void cls() { uart_puts("\033[H\033[J"); }
